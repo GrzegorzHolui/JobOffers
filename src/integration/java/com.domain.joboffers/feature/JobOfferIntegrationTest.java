0234e5db9369cec1f1fb50d349f929f447a05761
Grzegorz <grzesiu.holui@gmail.com>
2023-07-21 11:47:07 +0200

    add new test to integration

--- src/integration/java/com.domain.joboffers/feature/JobOfferIntegrationTest.java
+++ src/integration/java/com.domain.joboffers/feature/JobOfferIntegrationTest.java
@@ -1,41 +1,104 @@
-package com.domain.joboffers;
+package com.domain.joboffers.feature;
 
 
+import com.domain.joboffers.BaseIntegrationTest;
+import com.domain.joboffers.SampleJobOfferResponse;
+import com.domain.joboffers.infrastructure.offer.scheduler.OffersScheduler;
 import com.domain.joboffers.offerfacade.OfferFetchable;
 import com.domain.joboffers.offerfacade.dto.OfferResponseDto;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 public class JobOfferIntegrationTest extends BaseIntegrationTest implements SampleJobOfferResponse {
 
     @Autowired
     OfferFetchable offerFetchable;
 
+    @Autowired
+    OffersScheduler offersScheduler;
 
     @Test
-    public void f() {
-//   typical path: user want to see offers but have to be logged in and external server should have some offers
+    public void f() throws Exception {
+//   typical path: user want to see offers but have to be logged in and external server shouldtri have some offers
 //        step 1: there are no offers in external HTTP server (http://ec2-3-120-147-150.eu-central-1.compute.amazonaws.com:5057/offers)
-        // given
+        // given && when && then
         wireMockServer.stubFor(WireMock.get("/offers")
                 .willReturn(WireMock.aResponse()
                         .withStatus(HttpStatus.OK.value())
                         .withHeader("Content-Type", "application/json")
-                        .withBody(zeroJobOffer())));
-
-        // when
-        offerFetchable.fetchOffers();
+                        .withBody(threeJobOffer())));
 
 //        step 2: scheduler ran 1st time and made GET to external server and system added 0 offers to database
+        //given && when
+        List<OfferResponseDto> offerResponseDtos = offersScheduler.fetchAllOffersAndSaveAllIfNotExists();
+
+        //then
+        assertThat(offerResponseDtos).hasSize(3);
+
+
+//        post
+//        OfferRequestDto offerRequestDto = OfferRequestDto
+//                .builder()
+//                .linkToOffer("")
+//                .jobName("")
+//                .nameOfCompany("")
+//                .earnings(new EarningsRequestDto(BigDecimal.ZERO, BigDecimal.ONE))
+//                .build();
+//
+//        MvcResult mvcResult = mockMvc.perform(post("/inputOffer")
+//                        .content(new ObjectMapper().writeValueAsString(offerRequestDto))
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andReturn();
+//
+//
+//        assertThat(mvcResult.getResponse().getStatus()).isEqualTo(200);
+
+
 //        step 3: user tried to get JWT token by requesting POST /token with username=someUser, password=somePassword and system returned UNAUTHORIZED(401)
+
 //        step 4: user made GET /offers with no jwt token and system returned UNAUTHORIZED(401)
+
+
 //        step 5: user made POST /register with username=someUser, password=somePassword and system registered user with status OK(200)
 //        step 6: user tried to get JWT token by requesting POST /token with username=someUser, password=somePassword and system returned OK(200) and jwttoken=AAAA.BBBB.CCC
+
+
 //        step 7: user made GET /offers with header “Authorization: Bearer AAAA.BBBB.CCC” and system returned OK(200) with 0 offers
+
+        //given
+        MvcResult mvcResult = mockMvc.perform(get("/offers")).andReturn();
+
+        //when
+        List<OfferResponseDto> requestDtos = Arrays.stream(objectMapper.readValue(mvcResult.getResponse().getContentAsString(),
+                OfferResponseDto[].class)).toList();
+
+        //then
+        assertThat(mvcResult.getResponse().getStatus()).isEqualTo(200);
+        assertThat(requestDtos).hasSize(3);
+
+
+        //given
+        String id = requestDtos.get(0).id();
+
+        //when
+        MvcResult getResultWithParameter = mockMvc.perform(get("/offers/" + id)).andReturn();
+        OfferResponseDto offerResponseDto = objectMapper.readValue(getResultWithParameter.getResponse()
+                .getContentAsString(), OfferResponseDto.class);
+
+        //then
+        assertThat(offerResponseDto).isEqualTo(requestDtos.get(0));
+
 //        step 8: there are 2 new offers in external HTTP server
 //        step 9: scheduler ran 2nd time and made GET to external server and system added 2 new offers with ids: 1000 and 2000 to database
 //        step 10: user made GET /offers with header “Authorization: Bearer AAAA.BBBB.CCC” and system returned OK(200) with 2 offers with ids: 1000 and 2000
--- src/main/java/com/domain/joboffers/JobOffersApplication.java
+++ src/main/java/com/domain/joboffers/JobOffersApplication.java
@@ -1,25 +1,14 @@
 package com.domain.joboffers;
 
-import com.domain.joboffers.offerfacade.OfferFacade;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
-@EnableScheduling
-public class JobOffersApplication implements ApplicationRunner {
-    @Autowired
-    OfferFacade offerFacade;
+public class JobOffersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JobOffersApplication.class, args);
     }
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        offerFacade.fetchAllOffersAndSaveAllIfNotExists();
-    }
 }
