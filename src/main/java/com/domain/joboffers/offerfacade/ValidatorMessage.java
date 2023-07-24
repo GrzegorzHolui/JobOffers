16cd8b5290bfe9c79a25c5a25b0e4dfbe40451ba
Grzegorz <grzesiu.holui@gmail.com>
2023-07-24 11:48:54 +0200

    add the Enum "MAX_SALARY_IS_ZERO to ValidatorMessage

--- src/main/java/com/domain/joboffers/offerfacade/ValidatorMessage.java
+++ src/main/java/com/domain/joboffers/offerfacade/ValidatorMessage.java
@@ -3,6 +3,8 @@ package com.domain.joboffers.offerfacade;
 enum ValidatorMessage {
     MIN_SALARY_OR_MAX_SALARY_UNDER_ZERO("The min or max salary is under 0"),
     MIN_SALARY_HAS_BIGGER_VALUE_THAN_MAX_SALARY("The min value is bigger than max value"),
+
+    MAX_SALARY_IS_ZERO("MAX_SALARY_IS_ZERO"),
     LINK_IS_INVALID("The link is invalid");
 
     final String message;
