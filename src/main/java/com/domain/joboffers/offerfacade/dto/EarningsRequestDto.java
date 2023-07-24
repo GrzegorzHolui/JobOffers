34b4ccd31327008141464fccc5efd0a896061241
Grzegorz <grzesiu.holui@gmail.com>
2023-07-24 11:50:25 +0200

    add validation to EarningsRequestDto

--- src/main/java/com/domain/joboffers/offerfacade/dto/EarningsRequestDto.java
+++ src/main/java/com/domain/joboffers/offerfacade/dto/EarningsRequestDto.java
@@ -1,6 +1,13 @@
 package com.domain.joboffers.offerfacade.dto;
 
-import java.math.BigDecimal;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-public record EarningsRequestDto(BigDecimal minSalary, BigDecimal maxSalary) {
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public record EarningsRequestDto(
+        @NotEmpty(message = "salary should not be empty") @NotNull(message = "salary should not be null") BigDecimal minSalary,
+        @NotEmpty(message = "salary should not be empty") @NotNull(message = "salary should not be null") BigDecimal maxSalary) {
 }
